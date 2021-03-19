846a6364c56d10cb82a0338cd612ac50ecfc4f42
alex-mohylivskyi <alex.mohylivskyi@gmail.com>
2021-03-19 21:55:37 +0200

    first commit
    first commit text

---
+++ Assets/CubeSpawner3.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject           cubePrefabVar;
+    public List<GameObject>     gameObjectList;
+    public float                scalingFactor = 0.99f;
+    public int                  numCubes = 0;
+    void Start()
+    {
+        gameObjectList = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        numCubes++;
+        GameObject gObj = Instantiate<GameObject>(cubePrefabVar);
+
+        gObj.name = "Cube " + numCubes;
+        Color c = new Color(Random.value, Random.value, Random.value);
+        gObj.GetComponent<Renderer>().material.color = c;
+
+        gObj.transform.position = Random.insideUnitSphere;
+
+        gameObjectList.Add(gObj);
+
+        List<GameObject> removeList = new List<GameObject>();
+
+        // Обход кубиков в gameObjectList
+
+        foreach (GameObject goTemp in gameObjectList) {
+            float scale = goTemp.transform.localScale.x;
+            scale *= scalingFactor;
+            goTemp.transform.localScale = Vector3.one * scale;
+
+            if (scale <= 0.1f) {
+                removeList.Add(goTemp);
+            }
+        }
+
+        foreach (GameObject goTemp in removeList) {
+            gameObjectList.Remove(goTemp);
+            Destroy(goTemp);
+        }
+    }
+}
